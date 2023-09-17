f8bc3aee60d0fa25f8379e16d7b326399944febc
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-17 10:40:31 +0500

    10.2.4 Реализуйте явно следующий интерфейс и вызовите его метод в классе Program.
    public interface IWorker
    {
      public void Build();
    }

---
+++ InterfacesEdu/ImplicitInterface/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary>
+/// 10.2.4 Реализуйте явно следующий интерфейс и вызовите его метод в классе Program.
+/// </summary>
+namespace ImplicitInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+            worker.Build();
+        }
+    }
+    public interface IWorker
+    {
+        public void Build();
+    }
+    public class Worker : IWorker
+    {
+        public void Build()
+        {
+            Console.WriteLine("Неявно реализован метод Build интерфейса IWorker");
+        }
+    }
+}
