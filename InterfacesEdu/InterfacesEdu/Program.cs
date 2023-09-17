aae5888edcb7a3356ebd8cf7ad7b810c1cadc6e7
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-17 10:19:47 +0500

    10.1.4 Класс Manager реализует интерфейс IManager. Попытайтесь самостоятельно определить интерфейс IManager, исходя из данного кода.

---
+++ InterfacesEdu/InterfacesEdu/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+/// <summary>
+/// 10.1.4 Класс Manager реализует интерфейс IManager. Попытайтесь самостоятельно определить интерфейс IManager, исходя из данного кода.
+/// </summary>
+namespace InterfacesEdu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    interface IManager
+    {
+        public void Create();
+        public void Read();
+        public void Update();
+
+        public void Delete();
+    }
+    public class Manager : IManager
+    {
+        public void Create()
+        {
+
+        }
+
+        public void Read()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+    }
+
+}
