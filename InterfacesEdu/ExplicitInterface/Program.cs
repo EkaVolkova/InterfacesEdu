6cb3f16d6ec11bf9b6344bcd2597339c1360b925
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-17 10:42:49 +0500

    10.2.4 Реализуйте явно следующий интерфейс и вызовите его метод в классе Program.

--- InterfacesEdu/ExplicitInterface/Program.cs
+++ InterfacesEdu/ExplicitInterface/Program.cs
@@ -2,6 +2,7 @@
 
 /// <summary>
 /// 10.2.2 Используя теоретический материал из данного юнита, постарайтесь самостоятельно реализовать явную реализацию следующего интерфейса:
+/// 10.2.4 Реализуйте явно следующий интерфейс и вызовите его метод в классе Program 
 /// </summary>
 namespace ExplicitInterface
 {
@@ -11,6 +12,8 @@ namespace ExplicitInterface
         {
             Writer writer = new Writer();
             ((IWriter)writer).Write();
+            Worker worker = new Worker();
+            ((IWorker)worker).Build();
         }
     }
     public interface IWriter
@@ -22,6 +25,17 @@ namespace ExplicitInterface
         void IWriter.Write()
         {
             Console.WriteLine("Явно реализован метод Write интерфейса IWriter");
+        }
+    }
+    public interface IWorker
+    {
+        public void Build();
+    }
+    public class Worker : IWorker
+    {
+        void IWorker.Build()
+        {
+            Console.WriteLine("Явно реализован метод Build интерфейса IWorker");
         }
     }
 
