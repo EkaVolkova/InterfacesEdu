85b1f4bad536630f57fa06d6efbdf06d33f2a740
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-17 10:37:47 +0500

    10.2.2 Используя теоретический материал из данного юнита, постарайтесь самостоятельно реализовать явную реализацию следующего интерфейса:
    public interface IWriter
    {
      void Write();
    }

---
+++ InterfacesEdu/ExplicitInterface/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/// <summary>
+/// 10.2.2 Используя теоретический материал из данного юнита, постарайтесь самостоятельно реализовать явную реализацию следующего интерфейса:
+/// </summary>
+namespace ExplicitInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Writer writer = new Writer();
+            ((IWriter)writer).Write();
+        }
+    }
+    public interface IWriter
+    {
+        void Write();
+    }
+    public class Writer : IWriter
+    {
+        void IWriter.Write()
+        {
+            Console.WriteLine("Явно реализован метод Write интерфейса IWriter");
+        }
+    }
+
+}
