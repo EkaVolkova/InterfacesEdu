fd57d3641e1e567a2f07096674aff1e5a1f7a9f2
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-18 15:45:25 +0500

    10.3.1 Создайте класс FileManager и выполните в нём множественную реализацию интерфейсов:public interface IWriter
    {
      void Write();
    }
    
    public interface IReader
    {
      void Read();
    }
    
    public interface IMailer
    {
      void SendEmail();
    }

---
+++ InterfacesEdu/MultiplyInterfaces1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+/// <summary>
+/// Создайте класс FileManager и выполните в нём множественную реализацию интерфейсов:
+///     public interface IWriter 
+///     {
+///         void Write();
+///     }
+///     
+///     public interface IReader
+///     {
+///         void Read();
+///     }
+/// 
+///     public interface IMailer
+///     {
+///         void SendEmail();
+///     }
+/// 
+/// </summary>
+namespace MultiplyInterfaces1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public interface IWriter
+    {
+        void Write();
+    }
+
+    public interface IReader
+    {
+        void Read();
+    }
+
+    public interface IMailer
+    {
+        void SendEmail();
+    }
+    class FileManager : IWriter, IReader, IMailer
+    {
+        public void Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendEmail()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
