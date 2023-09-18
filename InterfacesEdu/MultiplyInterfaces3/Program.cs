d8b3dfbece7ba699fde094207adbfb31f2a70f83
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-18 15:52:49 +0500

    10.3.3 Даны два интерфейса:
    public interface IBook
    {
      void Read();
    }
    public interface IDevice
    {
      void TurnOn();
      void TurnOff();
    }
    Создайте класс ElectronicBook и выполните в нём множественную явную реализацию данных интерфейсов.

---
+++ InterfacesEdu/MultiplyInterfaces3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+/// <summary>
+/// Даны два интерфейса:
+///
+///public interface IBook
+///{
+///    void Read();
+///}
+
+///public interface IDevice
+///{
+///    void TurnOn();
+///    void TurnOff();
+///}
+///Создайте класс ElectronicBook и выполните в нём множественную явную реализацию данных интерфейсов.
+/// </summary>
+namespace MultiplyInterfaces3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public interface IBook
+    {
+        void Read();
+    }
+
+    public interface IDevice
+    {
+        void TurnOn();
+        void TurnOff();
+    }
+    class ElectronicBook : IBook, IDevice
+    {
+        void IBook.Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDevice.TurnOff()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDevice.TurnOn()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
