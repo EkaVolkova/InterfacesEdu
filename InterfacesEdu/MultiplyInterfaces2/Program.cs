d6069a3aa79f96e950e3abd543a345fbf1e29fe1
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-18 15:50:21 +0500

    10.3.2 Даны три интерфейса:
    public interface ICreatable
    {
      void Create();
    }
    public interface IDeletable
    {
      void Delete();
    }
    public interface IUpdatable
    {
      void Update();
    }
    Создайте класс Entity и выполните в нём множественную неявную реализацию данных интерфейсов.

---
+++ InterfacesEdu/MultiplyInterfaces2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+/// <summary>
+/// Даны три интерфейса:
+///     public interface ICreatable 
+///     {
+///         void Create();
+///     }
+///
+///     public interface IDeletable
+///     {
+///         void Delete();
+///     }
+///
+///     public interface IUpdatable
+///     {
+///         void Update();
+///     }
+/// Создайте класс Entity и выполните в нём множественную неявную реализацию данных интерфейсов.
+/// </summary>
+namespace MultiplyInterfaces2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public interface ICreatable
+    {
+        void Create();
+    }
+
+    public interface IDeletable
+    {
+        void Delete();
+    }
+
+    public interface IUpdatable
+    {
+        void Update();
+    }
+    class Entity : ICreatable, IDeletable, IUpdatable
+    {
+        public void Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
